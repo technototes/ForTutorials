57a27d2ad721838f1e5dee9d439e21383496018d
Kevin Frei <kevinfrei@hotmail.com>
2022-06-07 17:12:24 -0700

    Added try/catch around sleep

--- TeachCode/src/main/java/org/firstinspires/ftc/teachcode/TankDrive.java
+++ TeachCode/src/main/java/org/firstinspires/ftc/teachcode/TankDrive.java
@@ -3,7 +3,15 @@ package org.firstinspires.ftc.teachcode;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import java.util.*;
+
 public class TankDrive {
+    private void sleep(int ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (Exception e) {
+        }
+    }
+
     public DcMotorEx motorL;
     public DcMotorEx motorR;
 
@@ -11,23 +19,26 @@ public class TankDrive {
         this.motorL = motorL;
         this.motorR = motorR;
     }
+
     // For non-stick control system
     public void moveForwardButton(double power, int seconds) {
         motorL.setPower(power);
         motorR.setPower(power);
-        Thread.sleep(seconds * 1000);
+        sleep(seconds * 1000);
         pleaseStop();
     }
+
     // For non-stick control system
     public void pleaseStop() {
         motorL.setPower(0);
         motorR.setPower(0);
     }
+
     // For non-stick control system
     public void moveBackwardButton(double power, int seconds) {
         motorL.setPower(-power);
         motorR.setPower(-power);
-        Thread.sleep(seconds * 1000);
+        sleep(seconds * 1000);
         pleaseStop();
     }
 
@@ -35,10 +46,12 @@ public class TankDrive {
     public void motorLPower(double power) {
         motorL.setPower(power);
     }
+
     // For stick control system
     public void motorRPower(double power) {
         motorR.setPower(power);
     }
+
     public DcMotorEx getMotorL() {
         return motorL;
     }
