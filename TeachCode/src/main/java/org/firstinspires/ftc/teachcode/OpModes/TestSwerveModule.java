ea50b75c778c65ccc26bf634270b870ce9c9ae76
Kevin Frei <kevinfrei@hotmail.com>
2022-06-12 22:13:23 -0700

    Updating to have some diagnostic stuff. IMU not working.

--- TeachCode/src/main/java/org/firstinspires/ftc/teachcode/OpModes/TestSwerveModule.java
+++ TeachCode/src/main/java/org/firstinspires/ftc/teachcode/OpModes/TestSwerveModule.java
@@ -5,8 +5,8 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teachcode.SwerveModule;
 
 @TeleOp(name = "Test Swerve")
@@ -15,8 +15,9 @@ public class TestSwerveModule extends LinearOpMode {
     private static double POS_DEAD_ZONE = 0.1;
     private static double INV_ROOT_2 = 1.0 / Math.sqrt(2.0);
 
-    SwerveModule sm;
-    BNO055IMU imu;
+    private SwerveModule sm;
+    private BNO055IMU imu;
+    private ElapsedTime runtime = new ElapsedTime();
 
     private static double inRange(double low, double high, double val) {
         // Dumb & slow, but easy :D
@@ -43,34 +44,53 @@ public class TestSwerveModule extends LinearOpMode {
         imu = hardwareMap.get(BNO055IMU.class, "imu");
     }
 
+    // Notes: Gamepad stick orientiation is North -, West -, South +, East +
     @Override
     public void runOpMode() {
         hwinit();
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
         waitForStart();
+        runtime.reset();
+        telemetry.addData("Status", "Started");
+        telemetry.update();
         while (opModeIsActive()) {
             double rx = gamepad2.right_stick_x;
             double ry = gamepad2.right_stick_y;
             double y = gamepad2.left_stick_y;
             double x = gamepad2.left_stick_x;
+            double rx1 = gamepad1.right_stick_x;
+            double ry1 = gamepad1.right_stick_y;
+            double y1 = gamepad1.left_stick_y;
+            double x1 = gamepad1.left_stick_x;
+            telemetry.addData("Left 1", String.format("%f, %f", rx1, ry1));
+            telemetry.addData("Right 1", String.format("%f, %f", x1, y1));
+            telemetry.addData("Left 2", String.format("%f, %f", rx, ry));
+            telemetry.addData("Right 2", String.format("%f, %f", x, y));
+            /*
             if (Math.abs(rx) >= ROT_DEAD_ZONE) {
                 // This lets you rotate a little slower by default
                 // If you want to rotate faster, crank the y axis (either direction)
                 double magnitude = Math.sqrt(rx * rx + ry * ry) * INV_ROOT_2;
                 double sign = (rx < 0) ? -1.0 : 1.0;
                 // Square the value to scale non-linearly
-                sm.setDegrees(inRange(-180, 180, 360 * magnitude * magnitude * sign));
+                double degrees = 180 * magnitude * magnitude * sign;
+                sm.setDegrees(inRange(-180, 180, degrees));
+                telemetry.addData("Rotating", degrees);
             } else if (x >= POS_DEAD_ZONE || y >= POS_DEAD_ZONE) {
                 // I have no idea if the angle to take the IMU position into account
                 // for a driver-relative position is correct. The math should be *easy* but
                 // it really does need to be *correct* :)
                 float pos = imu.getAngularOrientation().toAngleUnit(AngleUnit.RADIANS).firstAngle;
-                double rad = inRange(-Math.PI, Math.PI, Math.atan2(y, x) - pos);
-                double pow = clamp(-1, 1, Math.sqrt(y * y + x * x));
-                sm.setPower(pow);
-                sm.setRadians(rad);
+                double pow = clamp(0, 1, Math.sqrt(y * y + x * x));
+                sm.setPower(pow * ((y < 0) ? -1 : 1));
+                telemetry.addData("Moving",  pow);
             } else {
                 sm.setPower(0);
             }
+             */
+            telemetry.addData("Elapsed Time", runtime.toString());
+            telemetry.update();
         }
     }
 }
