980fe8c590ffcdb0d8ab07a13047cf0fdd55659e
Technototes Laptop <technototes@outlook.com>
2022-06-17 17:34:12 -0700

    sensor code done-ish

---
+++ TeachCode/src/main/java/org/firstinspires/ftc/teachcode/TankDriveDemo.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teachcode;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+public class TankDriveDemo {
+    public DcMotorEx motorL;
+    public DcMotorEx motorR;
+
+
+}
