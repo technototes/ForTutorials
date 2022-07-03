f6f37d8df2e07707e2506c0567c95d5801408280
Kevin Frei <kevinfrei@hotmail.com>
2022-07-03 10:54:51 -0700

    Just my horrific attempts at implementing PIDF for a servo

--- TeachCode/src/main/java/org/firstinspires/ftc/teachcode/OpModes/SwerveModuleTester.java
+++ TeachCode/src/main/java/org/firstinspires/ftc/teachcode/OpModes/SwerveModuleTester.java
@@ -1,11 +1,17 @@
 package org.firstinspires.ftc.teachcode.OpModes;
 
+import android.util.Log;
+import android.util.Pair;
+
+import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teachcode.SwerveModule;
+
+import java.util.List;
 
 /*
 Motors: GoBilda 5202/3/4:
@@ -25,18 +31,26 @@ Analog Inputs:
 3: leftRearEncoder
 */
 
-@TeleOp(name = "Module Test")
+@TeleOp(name = "PIDTuner")
 public class SwerveModuleTester extends LinearOpMode {
     SwerveModule leftFront;
     SwerveModule rightFront;
     SwerveModule leftRear;
     SwerveModule rightRear;
+    private List<LynxModule> hubs = null;
 
     public void hwinit() {
-        leftFront = new SwerveModule(hardwareMap, "leftFront");
-        rightFront = new SwerveModule(hardwareMap, "rightFront");
-        leftRear = new SwerveModule(hardwareMap, "leftRear");
-        rightRear = new SwerveModule(hardwareMap, "rightRear");
+        // Set up 'bulk updates' mode...
+        if (hubs == null) {
+            hubs = hardwareMap.getAll(LynxModule.class);
+            for (LynxModule hub : hubs) {
+                hub.setBulkCachingMode(LynxModule.BulkCachingMode.AUTO);
+            }
+            leftFront = new SwerveModule(hardwareMap, "leftFront");
+            rightFront = new SwerveModule(hardwareMap, "rightFront");
+            leftRear = new SwerveModule(hardwareMap, "leftRear");
+            rightRear = new SwerveModule(hardwareMap, "rightRear");
+        }
     }
 
     public void encoderTestOpMode() throws InterruptedException {
@@ -143,38 +157,130 @@ public class SwerveModuleTester extends LinearOpMode {
         while (!isStarted()) idle();
         ElapsedTime et = new ElapsedTime();
         et.reset();
-        double nextAngle = 45;
-        boolean tri = false, sq = false, cr = false, cir = false;
-        boolean up = false;
+        // Stuff to trigger only when a button is first pressed...
+        boolean tri = false, sq = false, cross = false, cir = false;
+        boolean up = false, down = false, left = false, right = false;
+
+        int state = 0; // 0 -> setting, 1-> trying
+        // Setting state information
+        double bumpValue = 0.01;
+        double curValue = 0.01;
+        int which = 0; // 0 = P, 1 = I, 2 = D
+        // Trying state information
+        double targetAngle = 0.0;
+        double error = 0.0;
+        double value = 0.0;
+        double integral = 0;
+        double deriv = 0;
+        double delta = 45;
+        SwerveModule testMod = rightRear;
         while (opModeIsActive()) {
-            if (tri != gamepad1.triangle && tri) {
-                nextAngle = degrees(nextAngle + 45);
+            if (state == 0) {
+                // We're setting values to try
+                // left/right change the range of the number adding/subtracting
+                // up/down add/sub the number to the current value
+                if (gamepad1.dpad_left != left && !left) {
+                    bumpValue *= 10;
+                }
+                if (gamepad1.dpad_right != right && !right) {
+                    bumpValue /= 10;
+                }
+                if (gamepad1.dpad_up != up && !up) {
+                    curValue += bumpValue;
+                }
+                if (gamepad1.dpad_down != down && !down) {
+                    curValue -= bumpValue;
+                }
+                // Triangle: Set the value to the current element selected.
+                if (gamepad1.triangle != tri && !tri) {
+                    switch (which) {
+                        case 0:
+                            SwerveModule.servoPid.p = curValue;
+                            break;
+                        case 1:
+                            SwerveModule.servoPid.i = curValue;
+                            break;
+                        case 2:
+                            SwerveModule.servoPid.d = curValue;
+                            break;
+                        case 3:
+                            SwerveModule.servoPid.f = curValue;
+                            break;
+                    }
+                }
+                // Square: Move the previous element
+                if (gamepad1.square != sq && !sq) {
+                    which = (which + 3) % 4;
+                }
+                // Circle: Move the next element
+                if (gamepad1.circle != cir && !cir) {
+                    which = (which + 1) % 4;
+                }
+                // Bumpers change the angle-change to test with
+                if (gamepad1.left_bumper && et.milliseconds() > 200) {
+                    delta -= 1;
+                    et.reset();
+                }
+                if (gamepad1.right_bumper && et.milliseconds() > 200) {
+                    delta += 1;
+                    et.reset();
+                }
+                // Cross: Try the new values (on release so the try state has a chance to run)
+                if (gamepad1.cross != cross && cross) {
+                    testMod.resetPidData();
+                    targetAngle = (testMod.getAngle() + delta + 360) % 360;
+                    state = 1;
+                    et.reset();
+                }
+                tri = gamepad1.triangle;
+                cir = gamepad1.circle;
+                sq = gamepad1.square;
+                cross = gamepad1.cross;
+                down = gamepad1.dpad_down;
+                left = gamepad1.dpad_left;
+                right = gamepad1.dpad_right;
+                up = gamepad1.dpad_up;
+            } else {
+                Pair<Double, Double> errVal = testMod.headToDegrees(targetAngle);
+                error = errVal.first;
+                value = errVal.second;
+                if (gamepad1.cross || gamepad1.circle || gamepad1.triangle || gamepad1.square) {
+                    // Stop applying power first
+                    testMod.setRotatePower(0);
+                    // Now, wait for the button to be released before continuing
+                    while (gamepad1.cross || gamepad1.circle || gamepad1.triangle || gamepad1.square)
+                        ;
+                    state = 0;
+                }
             }
-            tri = gamepad1.triangle;
-            if (sq != gamepad1.square && sq) {
-                nextAngle = degrees(nextAngle - 45);
+            telemetry.addData("State", "%s (%1.0f° swing: use bumpers)",
+                    state == 0 ? "Setting" : "Trying", delta);
+            telemetry.addData((which == 0 && state == 0) ? "[>P<]" : "<[P]>",
+                    "%1.1e", SwerveModule.servoPid.p);
+            telemetry.addData((which == 1 && state == 0) ? "[>I<]" : "<[I]>",
+                    "%1.1e", SwerveModule.servoPid.i);
+            telemetry.addData((which == 2 && state == 0) ? "[>D<]" : "<[D]>",
+                    "%1.1e", SwerveModule.servoPid.d);
+            telemetry.addData((which == 3 && state == 0) ? "[>F<]" : "<[F]>",
+                    "%1.1e", SwerveModule.servoPid.f);
+            telemetry.addLine(String.format("Bump: %1.4e, Cur: %1.4e", bumpValue, curValue));
+            telemetry.addLine();
+            // Help text:
+            telemetry.addLine("                      ↑Cur+=Bump            △set");
+            telemetry.addLine("Bump*10←  →Bump/10\tprev□  ○next");
+            telemetry.addLine("                      ↓Cur-=Bump              ╳try it");
+            telemetry.addLine(state == 0 ? "" : "Pres any △□◯╳ to stop");
+            if (state == 1) {
+                double curAngle = testMod.getAngle();
+                telemetry.addData("Angles",
+                        "%3.1f->%3.1f (%3.1f)", curAngle, targetAngle, error);
+                telemetry.addData("Power", value);
+                Log.d("PIDF",
+                        String.format("%f\tTarget\t%3.1f\tCurrent\t%3.1f\tError\t%3.1f\tPower\t%1.8f",
+                                et.milliseconds(), targetAngle, curAngle,
+                                error, value));
             }
-            sq = gamepad1.square;
-            if (cr != gamepad1.cross && cr) {
-                nextAngle = degrees(nextAngle + 10);
-            }
-            cr = gamepad1.cross;
-            if (cir != gamepad1.circle && cir) {
-                nextAngle = degrees(nextAngle - 10);
-            }
-            cir = gamepad1.circle;
-            if (Math.abs(nextAngle - leftRear.getAngle()) > 5) {
-                leftRear.turnToDegrees(nextAngle, telemetry);
-            }
-            /*
-            telemetry.addData("target:", nextAngle);
-            telemetry.addData("rightFront", "%f", rightFront.getAngle());
-            telemetry.addData("leftFront", "%f", leftFront.getAngle());
-            telemetry.addData("rightRear", "%f", rightRear.getAngle());
-            telemetry.addData("leftRear", "%f", leftRear.getAngle());
-            telemetry.addData("Loop", et.toString());
-            et.reset();
-            telemetry.update();*/
+            telemetry.update();
         }
     }
 }
--- TeachCode/src/main/java/org/firstinspires/ftc/teachcode/SwerveModule.java
+++ TeachCode/src/main/java/org/firstinspires/ftc/teachcode/SwerveModule.java
@@ -1,20 +1,26 @@
 package org.firstinspires.ftc.teachcode;
 
+import android.util.Pair;
+
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.PIDCoefficients;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
-import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class SwerveModule {
     private DcMotorEx motor;
     private CRServo servo;
     private Lamprey2Encoder encoder;
     private static final double INV_PI = 1.0 / Math.PI;
-    private static final double angleError = 1.5;
-    public static PIDCoefficients servoPid = new PIDCoefficients(8e-3, 5e-15, 5e-15);
+    private static final double angleError = 3;
+
+    private void sleep(int ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (Exception e) {
+        }
+    }
 
     private double calcError(double targetDegrees) {
         // We have to deal with the 'around the edge' checks
@@ -31,44 +37,48 @@ public class SwerveModule {
     private double lastInt;
     private ElapsedTime lastErrTime;
     private ElapsedTime totalTime;
+    private ElapsedTime delayer;
 
     // Err is "# of degrees away from target"
     private double pidCalc(double err) {
-        double seconds = lastErrTime.seconds();
+        double timeDelta = lastErrTime.seconds();
         lastErrTime.reset();
-        double de_dt = (err - lastErr) / seconds; // derivative
-        double e_t = lastInt + err * seconds; // integral
+        boolean early = (totalTime.milliseconds() < 100);
+        double de_dt = early ? 0 : ((err - lastErr) / timeDelta); // derivative
+        double e_t = early ? 0 : (lastInt + err * timeDelta); // integral
+        double value = servoPid.p * err
+                + servoPid.i * e_t
+                + servoPid.d * de_dt
+                + servoPid.f * Math.signum(err);
+        err = lastErr;
         lastInt = e_t;
-        double value = servoPid.p * err + servoPid.i * e_t + servoPid.d * de_dt;
         return Math.max(-1.0, Math.min(1.0, value));
     }
 
-    public void turnToDegrees(double angle, Telemetry telemetry) {
+    public static PIDFCoefficients servoPid = new PIDFCoefficients(6e-3, 0.003, 0, 0.1);
+
+    public void resetPidData() {
+        lastErr = 0;
+        lastInt = 0;
         lastErrTime.reset();
         totalTime.reset();
-        double val = -2.0;
-        telemetry.addData("Starting:", angle);
-        int count = 0;
-        for (double err = calcError(angle);
-             Math.abs(err) > angleError && totalTime.milliseconds() < 2000;
-             err = calcError(angle), count++) {
-            val = pidCalc(err);
-            servo.setPower(val);
-            if (count < 11)
-                telemetry.addData("Err->val", "%3.0f->%1.4f", err, val);
-        }
-        servo.setPower(0);
-        telemetry.update();
-        servo.setPower(0);
+    }
+
+    public Pair<Double, Double> headToDegrees(double angle) {
+        double err = calcError(angle);
+        double val = pidCalc(err);
+        servo.setPower(val);
+        return new Pair<>(err, val);
     }
 
     public double getAngle() {
         return encoder.getAngle();
     }
-
+    /*
     public void turnToRadians(double angle, Telemetry telemetry) {
         turnToDegrees(angle * 180 * INV_PI, telemetry);
     }
+    */
 
     public void setRotatePower(double d) {
         servo.setPower(d);
@@ -85,7 +95,8 @@ public class SwerveModule {
         lastErr = 0;
         lastInt = 0;
         lastErrTime = new ElapsedTime();
-        totalTime=new ElapsedTime();
+        totalTime = new ElapsedTime();
+        delayer = new ElapsedTime();
     }
 
     public void manualDriveDegrees(double power, int angle) {
