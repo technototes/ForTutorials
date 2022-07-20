ceb19560c9da0b4ee00831f1b0e469f657371279
1toldyou <86680163+1toldyou@users.noreply.github.com>
2022-07-19 17:34:32 -0700

    incremental button

--- TeachCode/src/main/java/org/firstinspires/ftc/teachcode/OpModes/IntermediateServoDemo.java
+++ TeachCode/src/main/java/org/firstinspires/ftc/teachcode/OpModes/IntermediateServoDemo.java
@@ -5,12 +5,14 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teachcode.BasicServoCode;
+import org.firstinspires.ftc.teachcode.util.MathHelper;
 
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "IntermediateServoDemo")
 public class IntermediateServoDemo extends OpMode {
     private BasicServoCode servoCode;
     private Servo servo;
     private Gamepad gamepad;
+    private double inputtedPosition = 0.0;
 
     @Override
     public void init() {
@@ -31,13 +33,20 @@ public class IntermediateServoDemo extends OpMode {
     public void loop() {
         // Called every time the robot runs
         // Might be a bad solution since this will set the servo to the same position multiple times
-        // in the duration of the button being pressed
+        // in the duration of the button being set the position multiple times
+        // UPDATE: its fine to
         if (this.gamepad.dpad_left) {
             this.servoCode.toLeft();
         } else if (this.gamepad.dpad_right) {
             this.servoCode.toRight();
         } else if (this.gamepad.dpad_up) {
             this.servoCode.toMiddle();
+        } else if (this.gamepad.left_bumper){
+            this.inputtedPosition = MathHelper.clamp(this.inputtedPosition - 0.01, 0.0, 1.0);
+            this.servoCode.setPosition(this.inputtedPosition);
+        } else if (this.gamepad.right_bumper){
+            this.inputtedPosition = MathHelper.clamp(this.inputtedPosition + 0.01, 0.0, 1.0);
+            this.servoCode.setPosition(this.inputtedPosition);
         }
     }
 }
