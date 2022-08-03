f1b0493e322473a8941f43c3f05eee498e9298ce
1toldyou <86680163+1toldyou@users.noreply.github.com>
2022-08-02 17:12:21 -0700

    incremental button but only act once

--- TeachCode/src/main/java/org/firstinspires/ftc/teachcode/OpModes/IntermediateServoDemo.java
+++ TeachCode/src/main/java/org/firstinspires/ftc/teachcode/OpModes/IntermediateServoDemo.java
@@ -12,7 +12,8 @@ public class IntermediateServoDemo extends OpMode {
     private BasicServoCode servoCode;
     private Servo servo;
     private Gamepad gamepad;
-    private double inputtedPosition = 0.0;
+    private double currentServoPosition = 0.0;
+    private boolean isButtonJustPressed = true;
 
     @Override
     public void init() {
@@ -34,19 +35,32 @@ public class IntermediateServoDemo extends OpMode {
         // Called every time the robot runs
         // Might be a bad solution since this will set the servo to the same position multiple times
         // in the duration of the button being set the position multiple times
-        // UPDATE: its fine to
+        // UPDATE: its fine to keep setting the servo to the same position multiple times
         if (this.gamepad.dpad_left) {
             this.servoCode.toLeft();
         } else if (this.gamepad.dpad_right) {
             this.servoCode.toRight();
         } else if (this.gamepad.dpad_up) {
             this.servoCode.toMiddle();
-        } else if (this.gamepad.left_bumper){
-            this.inputtedPosition = MathHelper.clamp(this.inputtedPosition - 0.01, 0.0, 1.0);
-            this.servoCode.setPosition(this.inputtedPosition);
-        } else if (this.gamepad.right_bumper){
-            this.inputtedPosition = MathHelper.clamp(this.inputtedPosition + 0.01, 0.0, 1.0);
-            this.servoCode.setPosition(this.inputtedPosition);
+        } else if (this.gamepad.left_bumper) {
+            if (!this.isButtonJustPressed) {
+                this.isButtonJustPressed = true;
+                this.currentServoPosition = MathHelper.clamp(this.currentServoPosition - 0.05, 0.0, 1.0);
+                this.servoCode.setPosition(this.currentServoPosition);
+            }
+        } else if (this.gamepad.right_bumper) {
+            if (!this.isButtonJustPressed) {
+                this.isButtonJustPressed = true;
+                this.currentServoPosition = MathHelper.clamp(this.currentServoPosition + 0.05, 0.0, 1.0);
+                this.servoCode.setPosition(this.currentServoPosition);
+            }
+        } else {
+            this.isButtonJustPressed = false;
         }
+
+        telemetry.addData("IsButtonJustPressed", isButtonJustPressed);
+        telemetry.addData("CurrentServoPosition", currentServoPosition);
+        telemetry.addData("left_bumper", this.gamepad.left_bumper);
+        telemetry.addData("right_bumper", this.gamepad.right_bumper);
     }
 }
